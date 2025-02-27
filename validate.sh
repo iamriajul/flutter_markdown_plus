98cf3411d560706e0426950dc3cabd6762c1c869
kravoleksii <oleksii@foresightmobile.com>
2025-02-27 10:11:45 +0000

    -update validate.sh;

--- validate.sh
+++ validate.sh
@@ -12,5 +12,23 @@ verifyFlutter () {
   flutter pub get
   sh ./scripts/format.sh --set-exit-if-changed
   flutter analyze --no-pub .
-  very_good test
+
+  ###############################################################################
+  # CODE COVERAGE
+  ###############################################################################
+  dart pub global activate very_good_cli
+
+  rm -rf coverage
+  very_good test --no-optimization --coverage
+
+  lcov --remove coverage/lcov.info 'lib/core/data/local_image_data_source' 'lib/utils/image_picker_utils.dart' 'lib/core/domain/repositories/theme_repository.dart' 'lib/features/health/data/datasources/health_local_datasource_impl.dart' 'lib/core/data/repository/secure_storage_impl.dart' 'lib/core/data/repository/remote_config_repository_impl.dart' 'lib/core/data/app_review_wrapper.dart' 'lib/core/data/environment_variables.dart' 'lib/*/*.g.dart' 'lib/*/presentation/*.dart' 'lib/di/*.dart' 'lib/*/models/*.dart' 'lib/*/data/repositories/*.dart' -o coverage/lcov.info
+  genhtml -o coverage coverage/lcov.info
+
+  cd -
+}
+
+###############################################################################
+# INVOKE VERIFICATION FUNCTIONS
+###############################################################################
+verifyFlutter "."
 }
