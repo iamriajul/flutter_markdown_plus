88cdd04cc7e2d50211f973745c9943427fd829a8
kravoleksii <oleksii@foresightmobile.com>
2025-02-27 10:22:48 +0000

    -update validate.sh; -change flutter version for publish.yaml;

--- validate.sh
+++ validate.sh
@@ -21,9 +21,6 @@ verifyFlutter () {
   rm -rf coverage
   very_good test --no-optimization --coverage
 
-  lcov --remove coverage/lcov.info 'lib/core/data/local_image_data_source' 'lib/utils/image_picker_utils.dart' 'lib/core/domain/repositories/theme_repository.dart' 'lib/features/health/data/datasources/health_local_datasource_impl.dart' 'lib/core/data/repository/secure_storage_impl.dart' 'lib/core/data/repository/remote_config_repository_impl.dart' 'lib/core/data/app_review_wrapper.dart' 'lib/core/data/environment_variables.dart' 'lib/*/*.g.dart' 'lib/*/presentation/*.dart' 'lib/di/*.dart' 'lib/*/models/*.dart' 'lib/*/data/repositories/*.dart' -o coverage/lcov.info
-  genhtml -o coverage coverage/lcov.info
-
   cd -
 }
 
@@ -31,4 +28,3 @@ verifyFlutter () {
 # INVOKE VERIFICATION FUNCTIONS
 ###############################################################################
 verifyFlutter "."
-}
