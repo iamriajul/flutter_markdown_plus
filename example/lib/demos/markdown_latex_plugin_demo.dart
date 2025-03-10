7bff75d255ff3978a805294e53a56dfedb6fb1b7
kravoleksii <oleksii@foresightmobile.com>
2025-03-10 09:44:31 +0000

    -integrate flutter_markdown_plus_latex; -create demo for latex;

--- example/lib/demos/markdown_latex_plugin_demo.dart
+++ example/lib/demos/markdown_latex_plugin_demo.dart
@@ -6,51 +6,40 @@
 // ignore_for_file: avoid_implementing_value_types
 
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown_latex/flutter_markdown_latex.dart';
 import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
+import 'package:flutter_markdown_plus_latex/flutter_markdown_plus_latex.dart';
 import 'package:markdown/markdown.dart' as md;
 
 import '../shared/markdown_demo_widget.dart';
 
 // ignore_for_file: public_member_api_docs
 
-const String _markdownData = '''
-## ChatGPT Response
+const String _markdownData = r"""
+This is inline latex: $f(x) = \sum_{i=0}^{n} \frac{a_i}{1+x}$
 
-Welcome to ChatGPT! Below is an example of a response with Markdown and LaTeX code:
+This is block level latex:
 
-### Markdown Example
+$$
+c = \pm\sqrt{a^2 + b^2}
+$$
 
-You can use Markdown to format text easily. Here are some examples:
+This is inline latex with displayMode: $$f(x) = \sum_{i=0}^{n} \frac{a_i}{1+x}$$ and here is a very long text that should be in the same line.
 
-- **Bold Text**: **This text is bold**
-- *Italic Text*: *This text is italicized*
-- [Link](https://www.example.com): [This is a link](https://www.example.com)
-- Lists:
-  1. Item 1
-  2. Item 2
-  3. Item 3
+The relationship between the height and the side length of an equilateral triangle is:
 
-### LaTeX Example
+\[ \text{Height} = \frac{\sqrt{3}}{2} \times \text{Side Length} \]
 
-You can also use LaTeX for mathematical expressions. Here's an example:
+\[ \text{X} = \frac{1}{2} \times \text{Y} \times \text{Z} = \frac{1}{2} \times 9 \times \frac{\sqrt{3}}{2} \times 9 = \frac{81\sqrt{3}}{4} \]
 
-- **Equation**: \( f(x) = x^2 + 2x + 1 \)
-- **Integral**: \( \int_{0}^{1} x^2 \, dx \)
-- **Matrix**:
+The basic form of the Taylor series is:
 
-\[
-\begin{bmatrix}
-1 & 2 & 3 \\
-4 & 5 & 6 \\
-7 & 8 & 9
-\end{bmatrix}
-\]
+\[f(x) = f(a) + f'(a)(x-a) + \frac{f''(a)}{2!}(x-a)^2 + \frac{f'''(a)}{3!}(x-a)^3 + \cdots\]
 
-### Conclusion
+where \(f(x)\) is the function to be expanded, \(a\) is the expansion point, \(f'(a)\), \(f''(a)\), \(f'''(a)\), etc., are the first, second, third, and so on derivatives of the function at point \(a\), and \(n!\) denotes the factorial of \(n\).
 
-Markdown and LaTeX can be powerful tools for formatting text and mathematical expressions in your Flutter app. If you have any questions or need further assistance, feel free to ask!
-''';
+In particular, when \(a=0\), this expansion is called the Maclaurin series.
+
+""";
 
 const String _notes = '''
 # Latex & ChatGPT Response Demo
@@ -78,7 +67,7 @@ class MarkdownLatexPluginDemo extends StatelessWidget implements MarkdownDemoWid
   @override
   Future<String> get notes => Future<String>.value(_notes);
 
-  static MarkdownElementBuilder builder = LatexElementBuilder();
+  static LatexElementBuilder builder = LatexElementBuilder();
 
   @override
   Widget build(BuildContext context) {
@@ -86,10 +75,11 @@ class MarkdownLatexPluginDemo extends StatelessWidget implements MarkdownDemoWid
       body: SafeArea(
         child: Markdown(
           data: _markdownData,
-          builders: {'latex': builder},
+          selectable: true,
+          builders: <String, MarkdownElementBuilder>{'latex': builder},
           extensionSet: md.ExtensionSet(
-            [LatexBlockSyntax()],
-            [LatexInlineSyntax()],
+            <md.BlockSyntax>[LatexBlockSyntax()],
+            <md.InlineSyntax>[LatexInlineSyntax()],
           ),
         ),
       ),
